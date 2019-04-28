a9a7832fdf553edb93726b7b5239efbe5c079616
lucasbiel7 <lucasbiel7@icloud.com>
2019-04-28 17:46:36 -0300

    [Framework-componente] Lucas Gabriel: Foi adicionado o framework react native elements para facilitar o desenvolvimento do aplicativo
    Foi alterado a tela inicial, colocando os campos de usuário e senha com icones, é necessário adicionar uma borda, porque tá feio

--- app/pegaso/view/login/Login.view.js
+++ app/pegaso/view/login/Login.view.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text, TextInput } from "react-native";
 import globalStyle from "../../assets/styles/style";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
 
 export default class Login extends React.Component {
 
@@ -11,10 +13,12 @@ export default class Login extends React.Component {
     render() {
         return (
             <View style={globalStyle.container}>
-                <Text style={[globalStyle.whiteText]}>Usuário</Text>
-                <TextInput />
-                <Text style={[globalStyle.whiteText]}>Senha</Text>
-                <TextInput />
+                <Input placeholder=" Usuário" leftIcon={
+                    <Icon name="user" size={22} color="white" />
+                } />
+                <Input placeholder=" Senha" leftIcon={
+                    <Icon name="key" size={22} color="white" />
+                } />
             </View>
         );
     }
