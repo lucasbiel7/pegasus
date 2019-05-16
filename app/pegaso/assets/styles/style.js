ede1de182dbd524d4e009e9248fc0d22cda98e8d
phenriquesantos17 <phenriquesantos17@gmail.com>
2019-05-16 20:43:45 -0300

    Ajuste da tela de login para aparecer a imagem e os campos para realizar login

--- app/pegaso/assets/styles/style.js
+++ app/pegaso/assets/styles/style.js
@@ -2,25 +2,24 @@ import { StyleSheet } from "react-native";
 import Contantes from "./constantes.style";
 export default StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 3,
+        flexDirection: 'column',
         backgroundColor: 'white',
-        alignItems: "center",
         justifyContent: "center",
         paddingLeft: 10,
-        paddingRight: 10
+        paddingRight: 10,
     },
     logo: {
-        alignSelf: "stretch",
         resizeMode: "contain",
-        width: "50%",
-        marginLeft: "25%",
-        position: 'absolute',
+        maxWidth: '50%',
+        maxHeight: '50%',
+        marginLeft: '25%',
     },
     whiteText: {
         color: "white"
     },
     input: {
-        borderWidth: 1
+        paddingTop: 5,
     },
     button: {
         backgroundColor: Contantes.cores.laranja,
--- app/pegaso/view/login/Login.view.js
+++ app/pegaso/view/login/Login.view.js
@@ -14,13 +14,13 @@ export default class Login extends React.Component {
         return (
             <View style={globalStyle.container}>
                 <Image style={globalStyle.logo} source={require('./../../assets/image/logo.png')} />
-                <Input placeholder=" Usuário" leftIcon={
+                <Input placeholder=" Usuário" style={globalStyle.input} leftIcon={
                     <Icon name="user" size={22} color="black" />
                 } />
-                <Input placeholder=" Senha" style={{ color: 'black' }} leftIcon={
+                <Input placeholder=" Senha" style={globalStyle.input} leftIcon={
                     <Icon name="key" size={22} color="black" />
                 } />
-                <Button title="Entrar" icon={
+                <Button title="Entrar" style={{ marginTop: 4 }} icon={
                     <Icon name="sign-in" color="black" />
                 } containerStyle={globalStyle.button} />
             </View>
