98f0694284d844966ebe76a1c399a654845214f9
lucasbiel7 <lucasbiel7@icloud.com>
2019-06-02 19:35:49 -0300

    Fazendo autenticação do usuário no firebase

---
+++ app/pegasus/resource/usuario.class.js
@@ -0,0 +1,5 @@
+export default class Usuario {
+    nome;
+    login;
+    senha;
+}
--- app/pegasus/view/login/Login.view.js
+++ app/pegasus/view/login/Login.view.js
@@ -5,40 +5,48 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input, Button } from 'react-native-elements';
 import { NavigationActions, StackActions } from "react-navigation";
 import database from "../../database/firebase";
+import Usuario from "../../resource/usuario.class";
 
 export default class Login extends React.Component {
 
-    state = {
-        itens: []
-    }
+    usuario = new Usuario();
 
     constructor() {
         super();
     }
 
     login() {
-        // const navigateAction = StackActions.reset({
-        //     index: 0,
-        //     actions: [NavigationActions.navigate({ routeName: 'Dashboard' })]
-        // });
-        // this.props.navigation.dispatch(navigateAction);
-        database.ref('users').push({ name: 'teste' });
-        database.ref('users').on('value', (snapshot) => {
-            let data = snapshot.val();
-            let itens = Object.values(data);
-            this.setState({ itens });
-            console.log(itens);
+        if (this.usuario.login) {
+            database.ref('users').orderByChild('user').equalTo(this.usuario.login).on('value', (snapshot) => {
+                let data = snapshot.val();
+                if (data) {
+                    let itens = Object.values(data);
+                    itens.forEach(element => {
+                        if (element.password == this.usuario.senha) {
+                            this.redirectDashboard();
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    redirectDashboard() {
+        const navigateAction = StackActions.reset({
+            index: 0,
+            actions: [NavigationActions.navigate({ routeName: 'Dashboard' })]
         });
+        this.props.navigation.dispatch(navigateAction);
     }
 
     render() {
         return (
             <View style={globalStyle.container}>
                 <Image style={globalStyle.logo} source={require('./../../assets/image/logo.png')} />
-                <Input placeholder=" Usuário" style={globalStyle.padding5} leftIcon={
+                <Input placeholder=" Usuário" onChangeText={(login) => this.usuario.login = login} style={globalStyle.padding5} leftIcon={
                     <Icon name="user" size={22} color="black" />
                 } />
-                <Input placeholder=" Senha" type secureTextEntry={true} style={globalStyle.padding5} leftIcon={
+                <Input placeholder=" Senha" type secureTextEntry={true} onChangeText={(senha) => this.usuario.senha = senha} style={globalStyle.padding5} leftIcon={
                     <Icon name="key" size={22} color="black" />
                 } />
                 <Button title="Entrar" style={globalStyle.padding5} onPress={() => { this.login() }} />
