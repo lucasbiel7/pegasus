f4dfb194ea2a09e827086b1a5296c5f2c53ffed1
lucasbiel7 <lucasbiel7@icloud.com>
2019-06-02 20:50:43 -0300

    Ajutes nas telas de login para validar campos vazios e utilizar o firebase para autenticação

--- app/pegasus/view/login/Login.view.js
+++ app/pegasus/view/login/Login.view.js
@@ -10,25 +10,42 @@ import Usuario from "../../resource/usuario.class";
 export default class Login extends React.Component {
 
     usuario = new Usuario();
+    senhaError = null;
 
     constructor() {
         super();
     }
 
     login() {
-        if (this.usuario.login) {
-            database.ref('users').orderByChild('user').equalTo(this.usuario.login).on('value', (snapshot) => {
-                let data = snapshot.val();
-                if (data) {
-                    let itens = Object.values(data);
-                    itens.forEach(element => {
-                        if (element.password == this.usuario.senha) {
-                            this.redirectDashboard();
-                        }
-                    });
-                }
-            });
+        if (this.state) {
+            this.state.form = {};
         }
+        if (this.usuario.login) {
+            if (this.usuario.senha) {
+                database.ref('users').orderByChild('user').equalTo(this.usuario.login).on('value', (snapshot) => {
+                    let data = snapshot.val();
+                    if (data) {
+                        let itens = Object.values(data);
+                        let usuario = itens.filter(usuario => usuario.password == this.usuario.senha)[0];
+                        if (usuario) {
+                            this.redirectDashboard();
+                        } else {
+                            this.falhaLogin();
+                        }
+                    } else {
+                        this.falhaLogin();
+                    }
+                });
+            } else {
+                this.setState({ form: { senhaError: 'Campo de senha é obrigatório' } });
+            }
+        } else {
+            this.setState({ form: { usuarioErro: 'Campo de usuário é obrigatório' } });
+        }
+    }
+
+    falhaLogin() {
+        this.setState({ form: { senhaError: 'Usuário ou senha incorretos' } });
     }
 
     redirectDashboard() {
@@ -43,12 +60,17 @@ export default class Login extends React.Component {
         return (
             <View style={globalStyle.container}>
                 <Image style={globalStyle.logo} source={require('./../../assets/image/logo.png')} />
-                <Input placeholder=" Usuário" onChangeText={(login) => this.usuario.login = login} style={globalStyle.padding5} leftIcon={
-                    <Icon name="user" size={22} color="black" />
-                } />
-                <Input placeholder=" Senha" type secureTextEntry={true} onChangeText={(senha) => this.usuario.senha = senha} style={globalStyle.padding5} leftIcon={
-                    <Icon name="key" size={22} color="black" />
-                } />
+                <Input placeholder=" Usuário"
+                    onChangeText={(login) => this.usuario.login = login}
+                    errorMessage={this.state && this.state.form ? this.state.form.usuarioErro : ""}
+                    style={globalStyle.padding5} leftIcon={
+                        <Icon name="user" size={22} color="black" />
+                    } />
+                <Input placeholder=" Senha" type secureTextEntry={true} onChangeText={(senha) => this.usuario.senha = senha} style={globalStyle.padding5}
+                    errorMessage={this.state && this.state.form ? this.state.form.senhaError : ""}
+                    leftIcon={
+                        <Icon name="key" size={22} color="black" />
+                    } />
                 <Button title="Entrar" style={globalStyle.padding5} onPress={() => { this.login() }} />
             </View>
         );
